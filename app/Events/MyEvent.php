c2b8374c9ec8e2a3029232bc7dfdd868cf4a2997
nekozuki0114 <nekozuki.090@gmail.com>
2021-09-21 03:23:33 +0900

    データ送って受け取るだけのやつ

---
+++ app/Events/MyEvent.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MyEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        return ['my-channel'];
+				// return new Channel('my-event');
+    }
+
+    public function broadcastAs()
+    {
+        return 'my-event';
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => '/chat'], function () {
+    Route::get('/index', function () {
+        return view('pusher-index');
+    });
+
+    // 追加
+    Route::get('/test', function () {
+        event(new App\Events\MyEvent('hello world'));
+        return ['message' => 'send to message : hello world'];
+    });
+});
